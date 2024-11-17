b0a617b6dfa78dc5c2e327949a25c6f33b964d70
Guy Asaert <gasaert@gmail.com>
2024-11-17 17:17:39 +0000

    Some renaming to make consistent. Part 1 of day 3

---
+++ day_3/main.py
@@ -0,0 +1,53 @@
+from utils import read_lines
+from re import finditer
+from collections import namedtuple
+
+SAMPLE_INPUT = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+
+
+Symbol = namedtuple("Symbol", "row col symbol")
+Number = namedtuple("Number", "row col length number")
+
+def run():
+
+    symbols = []
+    numbers = []
+    number_near_symbol = []
+    # for row, row_of_data in enumerate(SAMPLE_INPUT.split('\n')):
+    for row, row_of_data in enumerate(read_lines(__file__)):
+        for number in finditer(r'\d+', row_of_data):
+            # print(f'Number: {number} at location: {location}')
+            numbers.append(Number(row, number.span()[0], number.span()[1] - number.span()[0], 
+                                  int(number.group(0))))
+
+        for symbol in finditer(r'[^.\d]', row_of_data):
+            # print(f'Symbol: {symbol} at location: {location}')
+            symbols.append(Symbol(row, symbol.span()[0], symbol.group(0)))
+
+    for number in numbers:
+            if number.col - 1 <= symbol.col <= number.col + number.length and \
+               number.row - 1 <= symbol.row <= number.row + 1:
+                print(f'Number: {number} is near symbol: {symbol}')
+                number_near_symbol.append(number.number)
+                break
+        if not number_near_symbol or number_near_symbol[-1] != number.number:
+            print(f'Number {number.number} on row {number.row} is not near any symbol')
+
+    print(f'{len(number_near_symbol)} numbers near symbols: total is {len(numbers)}')
+    print(f'Sum of numbers near symbols: {sum(number_near_symbol)}')
+    # 284222 is too low
+    # 300667 is too low
+
+
+if __name__ == '__main__':
+    run()
