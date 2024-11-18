44fef7b27a58e03317627938c7bf403883b69acf
Guy Asaert <gasaert@gmail.com>
2024-11-18 13:15:12 +0000

    Completed day 3

--- day_3/main.py
+++ day_3/main.py
@@ -34,17 +34,27 @@ def run():
             # print(f'Symbol: {symbol} at location: {location}')
             symbols.append(Symbol(row, symbol.span()[0], symbol.group(0)))
 
-    for number in numbers:
+    geared_total = 0
+    for symbol in symbols:
+        geared_numbers = []
+        for number in numbers:
             if number.col - 1 <= symbol.col <= number.col + number.length and \
                number.row - 1 <= symbol.row <= number.row + 1:
                 print(f'Number: {number} is near symbol: {symbol}')
                 number_near_symbol.append(number.number)
-                break
-        if not number_near_symbol or number_near_symbol[-1] != number.number:
-            print(f'Number {number.number} on row {number.row} is not near any symbol')
+                if symbol.symbol == '*':
+                    geared_numbers.append(number.number)
+
+        if len(geared_numbers) == 2:
+            geared_total += geared_numbers[0] * geared_numbers[1]
+
+        # if not number_near_symbol or number_near_symbol[-1] != number.number:
+        #     print(f'Number {number.number} on row {number.row} is not near any symbol')
 
     print(f'{len(number_near_symbol)} numbers near symbols: total is {len(numbers)}')
     print(f'Sum of numbers near symbols: {sum(number_near_symbol)}')
+
+    print(f'Geared total: {geared_total}')
     # 284222 is too low
     # 300667 is too low
 
